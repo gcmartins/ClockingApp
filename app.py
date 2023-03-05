beb87a65afed3e06fe26b65bd58ee610d0d9a4e9
Gustavo Martins <gucmartins@gmail.com>
2023-03-05 16:26:20 -0300

    Implement GUI with PyQt5

--- app.py
+++ app.py
@@ -1,86 +1,121 @@
 import datetime
 import pandas as pd
 import os.path
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton
+from PyQt5.QtCore import QTimer
 
 filename = 'work_hours.csv'
 header = ["Date", "Check In", "Check Out"]
 
 
-def record_check_in() -> None:
-    with open(filename, "a") as f:
-        current_time = datetime.datetime.now()
-        f.write("{},{},{}\n".format(current_time.date(), current_time.time(), ""))
+class WorkHoursApp(QWidget):
+    def __init__(self):
+        super().__init__()
 
+        self.timer = QTimer()
+        self.worked_hours = datetime.timedelta(0)
 
-def record_check_out() -> None:
-    with open(filename, "r+") as f:
-        lines = f.readlines()
-        if len(lines) == 0:
-            return
-        last_line = lines[-1].rstrip('\n').split(',')
-        if len(last_line) == 3 and last_line[2] == '':
-            last_line[2] = str(datetime.datetime.now().time())
-            lines[-1] = ','.join(last_line) + '\n'
-            f.seek(0)
-            f.writelines(lines)
+        self.setup_ui()
+
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)
+        self._is_checked_out = True
+        self.get_today_worked_hours()
+
+    def setup_ui(self):
+        self.setWindowTitle("Work Hours App")
+
+        self.lbl_time = QLabel(self.format_timedelta(self.worked_hours))
+        self.btn_check_in = QPushButton("Check In")
+        self.btn_check_out = QPushButton("Check Out")
+
+        self.update_buttons()
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.btn_check_in)
+        hbox.addWidget(self.btn_check_out)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.lbl_time)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
+        self.btn_check_in.clicked.connect(self.record_check_in)
+        self.btn_check_out.clicked.connect(self.record_check_out)
+
+    def update_buttons(self):
+        self.get_today_worked_hours()
+        if self._is_checked_out:
+            self.btn_check_in.setEnabled(True)
+            self.btn_check_out.setEnabled(False)
         else:
-            return
+            self.btn_check_in.setEnabled(False)
+            self.btn_check_out.setEnabled(True)
+
+    def record_check_in(self):
+        with open(filename, "a") as f:
+            current_time = datetime.datetime.now()
+            f.write("{},{},{}\n".format(current_time.date(), current_time.time(), ""))
+        self.update_buttons()
+
+    def record_check_out(self):
+        with open(filename, "r+") as f:
+            lines = f.readlines()
+            if len(lines) == 0:
+                return
+            last_line = lines[-1].rstrip('\n').split(',')
+            if len(last_line) == 3 and last_line[2] == '':
+                last_line[2] = str(datetime.datetime.now().time())
+                lines[-1] = ','.join(last_line) + '\n'
+                f.seek(0)
+                f.writelines(lines)
+            else:
+                return
+        self.update_buttons()
+
+    def update_time(self):
+        self.worked_hours = self.get_today_worked_hours()
+        self.lbl_time.setText(self.format_timedelta(self.worked_hours))
+        self.warn_if_overtime()
+
+    def get_today_worked_hours(self) -> datetime.timedelta:
+        df = pd.read_csv(filename, parse_dates=["Check In", "Check Out", "Date"])
+        today = datetime.date.today()
+        today_data = df[(df["Date"].dt.date == today) & (pd.isna(df['Check Out']) == False)]
+        duration = today_data["Check Out"] - today_data["Check In"]
+        todays_hours = duration.sum()
+
+        nan_check_out = df[pd.isna(df['Check Out'])]
+        self._is_checked_out = True
+        if len(today_data) == 0:
+            return datetime.timedelta(0)
+        elif not nan_check_out.empty:
+            self._is_checked_out = False
+            not_completed_hours = datetime.datetime.now() - nan_check_out["Check In"].iloc[0]
+            return todays_hours + not_completed_hours
+        else:
+            return todays_hours
+
+    def format_timedelta(self, td: datetime.timedelta) -> str:
+        seconds = td.seconds
+        hours, remainder = divmod(seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+
+    def warn_if_overtime(self) -> None:
+        todays_hours = self.get_today_worked_hours()
+        if todays_hours >= datetime.timedelta(hours=8):
+            self.lbl_time.setStyleSheet("color: red")
+        else:
+            self.lbl_time.setStyleSheet("color: black")
 
 
-def get_today_worked_hours() -> datetime.timedelta:
-    df = pd.read_csv(filename, parse_dates=["Check In", "Check Out"])
-    today = datetime.date.today()
-    today_data = df[df["Date"] == today]
-    if len(today_data) == 0:
-        return datetime.timedelta(0)
-    elif len(today_data) == 1 and pd.isna(today_data["Check Out"].iloc[0]):
-        return datetime.timedelta(0)
-    else:
-        today_data["Duration"] = today_data["Check Out"] - today_data["Check In"]
-        todays_hours = today_data["Duration"].sum()
-        return todays_hours
-
-
-def format_timedelta(td: datetime.timedelta) -> str:
-    seconds = td.seconds
-    hours, remainder = divmod(seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    return "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
-
-
-def warn_if_overtime() -> None:
-    todays_hours = get_today_worked_hours()
-    if todays_hours >= datetime.timedelta(hours=8):
-        print("Warning: You have worked {} today.".format(format_timedelta(todays_hours)))
-
-
-def main() -> None:
+if __name__ == '__main__':
     if not os.path.isfile(filename):
         with open(filename, "w") as f:
             f.write(','.join(header) + '\n')
-
-    print("Welcome to the work hours app.")
-    print("Press 'i' to record your check in time.")
-    print("Press 'o' to record your check out time.")
-    print("Press 'q' to quit the app.")
-
-    while True:
-        choice = input("Enter your choice: ")
-        if choice == "i":
-            record_check_in()
-        elif choice == "o":
-            record_check_out()
-        elif choice == "q":
-            print("Goodbye!")
-            break
-        else:
-            print("Invalid choice. Please try again.")
-            continue
-
-        todays_hours = get_today_worked_hours()
-        print("Total worked hours today: {}".format(format_timedelta(todays_hours)))
-        warn_if_overtime()
-
-
-if __name__ == "__main__":
-    main()
+    app = QApplication([])
+    widget = WorkHoursApp()
+    widget.show()
+    app.exec_()
