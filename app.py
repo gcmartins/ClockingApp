2755ef42922ab61e5c9c839bdbd8a5a163d4261d
Gustavo Martins <gucmartins@gmail.com>
2024-02-19 18:07:56 -0300

    Implement task clocking recording

--- app.py
+++ app.py
@@ -12,12 +12,22 @@ from pandas import DataFrame
 
 task_csv = 'task.csv'
 clocking_csv = 'work_hours.csv'
-header = ["Date", "Check In", "Check Out"]
+header = ["Date", "Task", "Check In", "Check Out"]
+
+
+class TaskUI:
+    def __init__(self, id: str, description: str, button: QPushButton):
+        self.id = id
+        self.description = description
+        self.button = button
+        self.button.setToolTip(self.description)
+        self.duration: datetime.timedelta = datetime.timedelta(0)
 
 
 class WorkHoursApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.started_task_id: Optional[str] = None
         self._dataframe: Optional[DataFrame] = None
         self.load_dataframe()
         self._is_checked_out = True
@@ -57,7 +67,7 @@ class WorkHoursApp(QWidget):
         self.hide()
 
     def load_dataframe(self):
-        self._dataframe = pd.read_csv(clocking_csv, parse_dates=["Check In", "Check Out", "Date"])
+        self._dataframe = pd.read_csv(clocking_csv, parse_dates=["Date", "Check In", "Check Out"])
 
     def tray_icon_activated(self, reason):
         if reason == QSystemTrayIcon.Trigger:
@@ -68,39 +78,48 @@ class WorkHoursApp(QWidget):
         self.setMinimumSize(200, 200)
 
         self.lbl_time = QLabel(self.format_timedelta(self.worked_hours))
-        self.btn_check_in = QPushButton("Check In")
+        self.create_task_buttons()
         self.btn_stop = QPushButton("STOP")
 
         self.update_buttons()
 
-        hbox = QHBoxLayout()
-        hbox.addWidget(self.btn_check_in)
-        hbox.addWidget(self.btn_stop)
-
         vbox = QVBoxLayout()
         vbox.addWidget(self.lbl_time)
-        vbox.addLayout(hbox)
+        for _, task in self.task_buttons.items():
+            vbox.addWidget(task.button)
+        vbox.addWidget(self.btn_stop)
 
         self.setLayout(vbox)
 
-        self.btn_check_in.clicked.connect(self.record_check_in)
         self.btn_stop.clicked.connect(self.record_check_out)
+
+    def create_task_buttons(self):
+        task_df = pd.read_csv(task_csv, names=["Task", "Description"], header=0)
+        self.task_buttons = {}
+        for _, task in task_df.iterrows():
+            task_id = task['Task']
+            btn_check_in = QPushButton(task_id)
+            btn_check_in.clicked.connect(self.record_check_in(task_id))
+            self.task_buttons[task_id] = TaskUI(task_id, task['Description'], btn_check_in)
 
     def update_buttons(self):
         self.get_today_worked_hours()
-        if self._is_checked_out:
-            self.btn_check_in.setEnabled(True)
-            self.btn_stop.setEnabled(False)
-        else:
-            self.btn_check_in.setEnabled(False)
-            self.btn_stop.setEnabled(True)
+        if self.started_task_id:
+            self.task_buttons[self.started_task_id].button.setEnabled(self._is_checked_out)
 
-    def record_check_in(self):
-        with open(clocking_csv, "a") as f:
-            current_time = datetime.datetime.now()
-            f.write("{},{},{}\n".format(current_time.date(), current_time.time(), ""))
-        self.load_dataframe()
-        self.update_buttons()
+        self.btn_stop.setEnabled(not self._is_checked_out)
+
+    def record_check_in(self, task_id: str):
+        def do_check_in():
+            if not self._is_checked_out:
+                self.record_check_out()
+            with open(clocking_csv, "a") as f:
+                current_time = datetime.datetime.now()
+                f.write("{},{},{},\n".format(current_time.date(), task_id, current_time.time().strftime("%H:%M:%S")))
+            self.load_dataframe()
+            self.started_task_id = task_id
+            self.update_buttons()
+        return do_check_in
 
     def record_check_out(self):
         with open(clocking_csv, "r+") as f:
@@ -108,8 +127,8 @@ class WorkHoursApp(QWidget):
             if len(lines) == 0:
                 return
             last_line = lines[-1].rstrip('\n').split(',')
-            if len(last_line) == 3 and last_line[2] == '':
-                last_line[2] = str(datetime.datetime.now().time())
+            if len(last_line) == 4 and last_line[3] == '':
+                last_line[3] = str(datetime.datetime.now().time().strftime("%H:%M:%S"))
                 lines[-1] = ','.join(last_line) + '\n'
                 f.seek(0)
                 f.writelines(lines)
@@ -136,6 +155,7 @@ class WorkHoursApp(QWidget):
         self._is_checked_out = True
         if not nan_check_out.empty:
             self._is_checked_out = False
+            self.started_task_id = nan_check_out["Task"].iloc[0]
             not_completed_hours = datetime.datetime.now() - nan_check_out["Check In"].iloc[0]
             return todays_hours + not_completed_hours
         else:
